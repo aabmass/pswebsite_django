f6692feeeda57f2223ea0bb5dd8db80fafddc409
Aaron Abbott <aabmass@gmail.com>
2015-07-24 01:30:53 -0400

    Changes to admin page
    Edit/create the actual poster image row from the poster admin page

--- pswebsite/admin.py
+++ pswebsite/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from pswebsite import models
+
+class PosterImageInline(admin.TabularInline):
+    model = models.PosterImage
+    extra = 2
+
+class PosterAdmin(admin.ModelAdmin):
+    inlines = [PosterImageInline]
+
+# TODO: reorder the fields for dimension so it is correct width x height
+
+admin.site.register(models.Poster, PosterAdmin)
+admin.site.register(models.PosterDimension)
