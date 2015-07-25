152c6d7de6836f426de7189415e7163ec62e23e1
Aaron Abbott <aabmass@gmail.com>
2015-07-24 21:46:04 -0400

    Admin autosave the user_creator
    Automatically choose the user submitting the request as the
    creator_user of a Poster object

--- pswebsite/admin.py
+++ pswebsite/admin.py
@@ -9,7 +9,15 @@ class PosterImageInline(admin.TabularInline):
 class PosterAdmin(admin.ModelAdmin):
     inlines = [PosterImageInline]
 
+    def save_model(self, request, obj, form, change):
+        """When creating a new object, set the user_creator field.
+        """
+        if not change:
+            obj.user_creator = request.user
+            obj.save()
+
 # TODO: reorder the fields for dimension so it is correct width x height
 
 admin.site.register(models.Poster, PosterAdmin)
 admin.site.register(models.PosterDimension)
+admin.site.register(models.PosterImage)
