613b5a4d031f62893cb5785443cee71f2f084fb0
Aaron Abbott <aabmass@gmail.com>
2015-07-27 01:39:15 -0400

    Fixed urls stopping the mobile breakpoints
    Fixed the urls in init.js

--- pswebsite/static/pswebsite/js/init.js
+++ pswebsite/static/pswebsite/js/init.js
@@ -10,36 +10,36 @@
 		reset: 'full',
 		breakpoints: {
 			global: {
-				href: '/static/css/style.css',
+				href: '/static/pswebsite/css/style.css',
 				containers: 1400,
 				grid: { gutters: ['2em', 0] }
 			},
 			xlarge: {
 				media: '(max-width: 1680px)',
-				href: '/static/css/style-xlarge.css',
+				href: '/static/pswebsite/css/style-xlarge.css',
 				containers: 1200
 			},
 			large: {
 				media: '(max-width: 1280px)',
-				href: '/static/css/style-large.css',
+				href: '/static/pswebsite/css/style-large.css',
 				containers: 960,
 				grid: { gutters: ['1.5em', 0] },
 				viewport: { scalable: false }
 			},
 			medium: {
 				media: '(max-width: 980px)',
-				href: '/static/css/style-medium.css',
+				href: '/static/pswebsite/css/style-medium.css',
 				containers: '90%!'
 			},
 			small: {
 				media: '(max-width: 736px)',
-				href: '/static/css/style-small.css',
+				href: '/static/pswebsite/css/style-small.css',
 				containers: '90%!',
 				grid: { gutters: ['1.25em', 0] }
 			},
 			xsmall: {
 				media: '(max-width: 480px)',
-				href: '/static/css/style-xsmall.css'
+				href: '/static/pswebsite/css/style-xsmall.css'
 			}
 		},
 		plugins: {
